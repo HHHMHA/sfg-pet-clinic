0d26f1e82a10d97e64ed0a77b45ed712e25f3886
J2mF <mp4-12cs5@hotmail.com>
2020-06-02 18:46:20 +0300

    Initial commit

---
+++ src/test/java/thekiddos/j2mf/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package thekiddos.j2mf.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
