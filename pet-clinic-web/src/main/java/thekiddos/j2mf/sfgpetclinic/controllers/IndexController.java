5d7dfbbf43cc3f02bb02bb2426892c913273decb
J2mF <mp4-12cs5@hotmail.com>
2020-06-15 00:18:35 +0300

    Added Pet Clinic Index Page and Controller, fixed wrong dependencies on pom files. Closes #8.

---
+++ pet-clinic-web/src/main/java/thekiddos/j2mf/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package thekiddos.j2mf.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
