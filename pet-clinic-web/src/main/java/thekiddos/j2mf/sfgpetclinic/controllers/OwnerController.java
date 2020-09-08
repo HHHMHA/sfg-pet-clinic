cd0a60eccfea79cec35d6913c6019af9668d9531
J2mF <mp4-12cs5@hotmail.com>
2020-09-08 13:46:45 +0300

    Implemented List Owners on Owner Index Page. Closes #16

--- pet-clinic-web/src/main/java/thekiddos/j2mf/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/thekiddos/j2mf/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package thekiddos.j2mf.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import thekiddos.j2mf.sfgpetclinic.services.OwnerService;
 
 @Controller
 @RequestMapping(value = "/owners")
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController( OwnerService ownerService ) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping
-    public String listOwners() {
+    public String listOwners( Model model ) {
+        model.addAttribute( "owners", ownerService.findAll() );
         return "owners/index";
     }
 }
