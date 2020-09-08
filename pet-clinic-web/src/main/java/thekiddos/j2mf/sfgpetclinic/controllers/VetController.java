2c72be37643194609005c3cf614950cec6f8f674
J2mF <mp4-12cs5@hotmail.com>
2020-09-08 13:50:03 +0300

    Implemented List All vets on vet index page. Closes #15

--- pet-clinic-web/src/main/java/thekiddos/j2mf/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/thekiddos/j2mf/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package thekiddos.j2mf.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import thekiddos.j2mf.sfgpetclinic.services.VetService;
 
 @Controller
 @RequestMapping(value = "/vets")
 public class VetController {
+    private final VetService vetService;
+
+    public VetController( VetService vetService ) {
+        this.vetService = vetService;
+    }
+
     @GetMapping
-    public String listVets() {
+    public String listVets( Model model ) {
+        model.addAttribute( "vets", vetService.findAll() );
         return "vets/index";
     }
 }
