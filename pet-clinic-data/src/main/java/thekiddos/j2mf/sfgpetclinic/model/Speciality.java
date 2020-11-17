8d0049c5fde5c3a20cab36eeed6c72ba5d7828a2
J2mF <mp4-12cs5@hotmail.com>
2020-11-17 17:23:53 +0200

    Create Vet Specialty Entity, Associate To Vet Closes #27

---
+++ pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,8 @@
+package thekiddos.j2mf.sfgpetclinic.model;
+
+import lombok.Data;
+
+@Data
+public class Speciality extends BaseEntity {
+    private String description;
+}
--- pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/model/Vet.java
@@ -1,4 +1,10 @@
 package thekiddos.j2mf.sfgpetclinic.model;
 
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class Vet extends Person {
+    private Set<Speciality> specialities;
 }
