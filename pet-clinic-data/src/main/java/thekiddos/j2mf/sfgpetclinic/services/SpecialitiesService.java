bb634c5aa8dcf01231029d61d82b2ec2aa726854
J2mF <mp4-12cs5@hotmail.com>
2020-11-24 19:16:13 +0200

    Create Speciality Map Service Impl Closes #31

---
+++ pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package thekiddos.j2mf.sfgpetclinic.services;
+
+import thekiddos.j2mf.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/services/map/SpecialitiesServiceMap.java
@@ -0,0 +1,33 @@
+package thekiddos.j2mf.sfgpetclinic.services.map;
+
+import thekiddos.j2mf.sfgpetclinic.model.Speciality;
+import thekiddos.j2mf.sfgpetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+public class SpecialitiesServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Speciality findById( Long id ) {
+        return super.findById( id );
+    }
+
+    @Override
+    public Speciality save( Speciality entity ) {
+        return super.save( entity );
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById( Long id ) {
+        super.deleteById( id );
+    }
+
+    @Override
+    public void delete( Speciality entity ) {
+        super.delete( entity );
+    }
+}
