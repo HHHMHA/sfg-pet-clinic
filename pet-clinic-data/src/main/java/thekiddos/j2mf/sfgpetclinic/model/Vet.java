5235a86426eb53d7a80b01a91ef53fbef91a5eba
J2mF <mp4-12cs5@hotmail.com>
2020-12-29 15:15:31 +0200

    Fix typo

--- pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/model/Vet.java
@@ -11,7 +11,7 @@ import java.util.Set;
 @Table( name = "vets" )
 public class Vet extends Person {
     @ManyToMany( fetch = FetchType.EAGER )
-    @JoinTable( name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id")
-    @inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    @JoinTable( name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+    inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 }
