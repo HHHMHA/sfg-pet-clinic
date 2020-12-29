d87c7964d0061684885c444cfad43608cce82b1d
J2mF <mp4-12cs5@hotmail.com>
2020-12-29 15:14:28 +0200

    Update vet with JPA entity mappings Closes #37

--- pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/model/Speciality.java
@@ -2,7 +2,14 @@ package thekiddos.j2mf.sfgpetclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Data
+@Entity
+@Table( name = "specialities" )
 public class Speciality extends BaseEntity {
+    @Column( name = "description" )
     private String description;
 }
--- pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/model/Vet.java
@@ -2,10 +2,16 @@ package thekiddos.j2mf.sfgpetclinic.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@Entity
+@Table( name = "vets" )
 public class Vet extends Person {
+    @ManyToMany( fetch = FetchType.EAGER )
+    @JoinTable( name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id")
+    @inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 }
