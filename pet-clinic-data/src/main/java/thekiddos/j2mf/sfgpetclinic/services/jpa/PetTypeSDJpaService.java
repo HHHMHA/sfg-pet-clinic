1bcc823ea46e388d872bcad41be1a4e94878af14
J2mF <mp4-12cs5@hotmail.com>
2021-04-14 17:59:21 +0300

    Create Spring Data JPA Pet Type Service (Fixes #42)

---
+++ pet-clinic-data/src/main/java/thekiddos/j2mf/sfgpetclinic/services/jpa/PetTypeSDJpaService.java
@@ -0,0 +1,51 @@
+package thekiddos.j2mf.sfgpetclinic.services.jpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import thekiddos.j2mf.sfgpetclinic.model.PetType;
+import thekiddos.j2mf.sfgpetclinic.repositories.PetTypeRepository;
+import thekiddos.j2mf.sfgpetclinic.services.PetService;
+
+import java.util.Set;
+import java.util.HashSet;
+
+@Service
+@Profile( value = "jpa")
+public class PetTypeSDJpaService implements PetService {
+    private final PetTypeRepository petTypeRepository;
+    
+    @Autowired
+    public PetTypeSDJpaService( PetTypeRepository petTypeRepository ) {
+        this.petTypeRepository = petTypeRepository;
+    }
+    
+    @Override
+    PetType findById( Long id ) {
+        return petTypeRepository.findById( id ).orElse( null );
+    }
+
+    @Override
+    PetType save( PetType petType ) {
+        return petTypeRepository.save( petType );
+    }
+
+    @Override
+    Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        
+        petTypeRepository.findAll().forEach(petTypes:add);
+        
+        return petTypes;
+    }
+
+    @Override
+    void delete( PetType petType ) {
+        petTypeRepository.delete( vet );
+    }
+    
+    @Override
+    void deleteById( Long id ) {
+        petTypeRepository.deleteById( id );
+    }
+}
